d28577248dad077348d12116e1b0cb9e54511d1c
SamiZeinAI <zeins20@mycu.concord.edu>
2024-03-15 23:06:24 -0400

    Added boilerplate

---
+++ react-app/src/Developers.jsx
@@ -0,0 +1,17 @@
+import "./Developers.css";
+export default function Developers() {
+    return (
+        <>
+           
+            <div className="devs-header">
+                <img src="./assets/react.svg"  />
+                <h1 className="devs-header-title">Developers</h1>
+            </div>
+            <h2>Your next collaborator could be right here.</h2>
+            <div className="users-section">
+
+            </div>
+
+        </>
+    );
+}
---
+++ react-app/src/UserSelected.jsx
@@ -0,0 +1,5 @@
+export default function UserSelected() {
+    return (
+        <></>
+    );
+}
